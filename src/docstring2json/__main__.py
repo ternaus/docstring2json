4dde387213f772c728fc6584a65f317056784280
Vladimir Iglovikov <iglovikov@gmail.com>
2025-05-08 16:55:34 -0700

    Now save to json

--- src/docstring2json/__main__.py
+++ src/docstring2json/__main__.py
@@ -9,8 +9,7 @@ from pathlib import Path
 src_dir = Path(__file__).parent.parent.parent / "src"
 sys.path.insert(0, str(src_dir))
 
-from docstring2tsx.converter import file_to_json
-
+from docstring2json.converter import file_to_json
 from utils.shared import process_package
 
 # Configure logging
